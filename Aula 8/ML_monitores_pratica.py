b6a52d93dfb8a4467c3c4fc6a67f1be5b5e09505
Guilherme Loan Schneider <guilhermeloan@alunos.utfpr.edu.br>
2025-01-07 13:54:59 -0300

    Adição dos comentários nos arquivos de aula e prática

--- Aula 8/ML_monitores_pratica.py
+++ Aula 8/ML_monitores_pratica.py
@@ -1,26 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Utilização de uma input para filtrar os monitores de acordo com a marca inserida
 print('Insira a marca do monitor que você deseja pesquisar: ')
 monitor_brand = input('>')
 print(f'Filtrando monitores da marca: {monitor_brand}')
 
 def find_monitor():
+    # Seção do acesso, processamento do HTML, e filtragem dos monitores
     html_text = requests.get('https://lista.mercadolivre.com.br/informatica/monitores-acessorios/monitores/monitor-180hz_CustoFrete_Gratis_NoIndex_True#applied_filter_id%3Dshipping_cost_highlighted_free%26applied_filter_name%3DCusto+do+frete%26applied_filter_order%3D2%26applied_value_id%3Dfree%26applied_value_name%3DGr%C3%A1tis%26applied_value_order%3D1%26applied_value_results%3D158%26is_custom%3Dfalse').text
     soup = BeautifulSoup(html_text, 'lxml')
     monitor = soup.find_all('li', class_='ui-search-layout__item')
 
+    # Iteração para pegar as informações de cada monitor, e salvar em um arquivo .txt
     for index, item in enumerate(monitor):
+        # Pega o nome, preço, parcelamento, e link do produto
         product_name = item.find('a', class_='').text
         product_price = item.find('span', class_='andes-money-amount__fraction').text
-        
         installment_section = item.find('span', class_='poly-price__installments poly-text-primary')
+        # Verificação se o parcelamento está disponível
+        # O site escolhido possui as informações em spans diferentes, necessitando de um tratamento diferente
         if installment_section:
             installments = installment_section.text.split('em')[1].strip() if 'em' in installment_section.text else installment_section.text.strip()
         else:
             installments = 'Parcelamento indisponível'
         
         more_info = item.h2.a['href']
+        
         # if monitor_brand in product_name:
         #     print(f'Nome do Produto: {product_name.strip()}')
         #     print(f'Preço: R${product_price.strip()}')
@@ -28,6 +34,7 @@ def find_monitor():
         #     print(f'Mais Informações: {more_info}')
         #     print('===============================')
         
+        # Verificação se o nome do produto contém a marca inserida, caso contenha, salva o arquivo
         if monitor_brand in product_name:
             with open(f'Aula 8/monitores/{index}.txt', 'w') as f:
                 f.write(f'Nome do Produto: {product_name.strip()} \n')
@@ -38,4 +45,4 @@ def find_monitor():
             print(f'Arquivo salvo: {index}')
 
 if __name__ == '__main__':
-    find_monitor()
+    find_monitor() # Chamada da função para filtrar os monitores
--- Aula 8/TimesJobs_aula.py
+++ Aula 8/TimesJobs_aula.py
@@ -2,29 +2,39 @@ import time
 from bs4 import BeautifulSoup
 import requests
 
+# Utilização de uma input para filtrar as vagas de acordo com a inserida
 print('Insira habilidades que você não é familiarizado: ')
 unfamiliar_skills = input('>')
 print(f'Filtrando vagas que não contém: {unfamiliar_skills}')
 
 def find_jobs():
+    # Utilização do requests para pegar o conteúdo HTML da página
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+    # Utilização do BeautifulSoup para fazer o parsing do conteúdo HTML
     soup = BeautifulSoup(html_text, 'lxml')
+    # Utilização do find_all para pegar as seções que possuem todas as vagas
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
+    # Iteração para pegar as informações de cada vaga, e salvar em um arquivo .txt com o index da vaga
     for index, job in enumerate(jobs):
         published_date = job.find('span', class_='sim-posted').span.text
+        # Verificação se a vaga foi publicada recentemente
         if 'few' in published_date:
             company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
             skills = job.find('div', class_='more-skills-sections').text.replace(' ', '')
+            # Pega o link da vaga a partir da href 
             more_info = job.header.h2.a['href']
+            # Verificação se a vaga não contém as habilidades inseridas, caso não contenha, salva o arquivo
             if unfamiliar_skills not in skills:
+                # Faz a escrita do arquivo .txt com as informações da vaga
                 with open(f'posts/{index}.txt', 'w') as f:
                     f.write(f'Company Name: {company_name.strip()} \n')
                     f.write(f'Required Skills: {skills.strip()} \n')
                     f.write(f'More Info: {more_info}')
                 print(f'File saved: {index}')
 
-if __name__ == '__main__':
+# Loop para filtrar as vagas a cada 15 minutos
+if __name__ == '__main__': # utilização do __main__ para garantir que o código só será executado se for o arquivo principal
     while True:
         find_jobs()
         print('Filtrando vagas a cada 15 minutos...')
